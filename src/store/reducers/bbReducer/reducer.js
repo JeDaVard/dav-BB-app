1d2842d0826aa51caa6cf6cebacf0ef405a32a42
Davit Vardanyan <jedavard@gmail.com>
2020-04-06 00:14:34 +0400

    reducer refactoring

--- src/store/reducers/bbReducer/reducer.js
+++ src/store/reducers/bbReducer/reducer.js
@@ -1,4 +1,5 @@
 import * as actionTypes from '../../actions/bbActions/types';
+import { updateObject } from '../unility';
 
 const initialState = {
     ingredients: null,
@@ -14,43 +15,47 @@ const INGREDIENT_PRICE = {
     meat: 5,
 };
 
+const fetchIngredients = (state, action) => (
+    updateObject(state, {
+        ingredients: action.ingredients,
+        totalPrice: 4,
+        error: null,
+    })
+);
+const fetchIngredientsError = (state, action) => (
+    updateObject(state, { error: action.error })
+);
+const addIngredient = (state, action) => {
+    const newIncrState = {
+        ingredients: {
+            ...state.ingredients,
+            [action.ingredient]: state.ingredients[action.ingredient] + 1
+        },
+        totalPrice:
+            state.totalPrice + INGREDIENT_PRICE[action.ingredient],
+    };
+    return updateObject(state, { ...newIncrState })
+};
+const removeIngredient = (state, action) => {
+    const newIncrState = {
+        ingredients: {
+            ...state.ingredients,
+            [action.ingredient]: state.ingredients[action.ingredient] - 1
+        },
+        totalPrice:
+            state.totalPrice - INGREDIENT_PRICE[action.ingredient],
+    };
+    return updateObject(state, { ...newIncrState })
+};
+
+
+
 export default (state = initialState, action) => {
     switch (action.type) {
-        case actionTypes.FETCH_INGREDIENTS:
-            return {
-                ...state,
-                ingredients: action.ingredients,
-                totalPrice: 4,
-                error: null,
-            };
-        case actionTypes.FETCH_INGREDIENTS_ERROR:
-            return {
-                ...state,
-                error: action.error,
-            };
-        case actionTypes.ADD_INGREDIENT:
-            return {
-                ...state,
-                ingredients: {
-                    ...state.ingredients,
-                    [action.ingredient]:
-                        state.ingredients[action.ingredient] + 1,
-                },
-                totalPrice:
-                    state.totalPrice + INGREDIENT_PRICE[action.ingredient],
-            };
-        case actionTypes.REMOVE_INGREDIENT:
-            return {
-                ...state,
-                ingredients: {
-                    ...state.ingredients,
-                    [action.ingredient]:
-                        state.ingredients[action.ingredient] - 1,
-                },
-                totalPrice:
-                    state.totalPrice - INGREDIENT_PRICE[action.ingredient],
-            };
-        default:
-            return state;
+        case actionTypes.FETCH_INGREDIENTS: return fetchIngredients(state, action);
+        case actionTypes.FETCH_INGREDIENTS_ERROR: return fetchIngredientsError(state, action);
+        case actionTypes.ADD_INGREDIENT: return addIngredient(state, action);
+        case actionTypes.REMOVE_INGREDIENT: return removeIngredient(state, action);
+        default: return state;
     }
 };
--- src/store/reducers/orderReducer/reducer.js
+++ src/store/reducers/orderReducer/reducer.js
@@ -1,34 +1,30 @@
-import * as actions from '../../actions/orderActions/types'
+import * as actions from '../../actions/orderActions/types';
+import { updateObject } from "../unility";
 
 const initialState = {
     orders: [],
     error: null
 };
+
+const fetchOrders = (state, action) => (
+    updateObject(state, { orders: action.orders, error: null} )
+);
+const fetchOrdersError = (state, action) => (
+    updateObject(state, { error: action.error } )
+);
+const purchaseSuccess = (state, action) => (
+    updateObject(state, { orders: state.orders.concat({...action.order, id: action.id}), error: null} )
+);
+const purchaseFailed = (state, action) => (
+    updateObject(state, { error: action.error } )
+);
+
 export default (state = initialState, action) => {
     switch (action.type) {
-        case actions.FETCH_ORDERS:
-            return {
-                ...state,
-                orders: action.orders,
-                error: null
-            };
-        case actions.FETCH_ORDERS_ERROR:
-            return {
-                ...state,
-                error: action.error
-            };
-        case actions.PURCHASE_SUCCESS:
-            return {
-                ...state,
-                orders: state.orders.concat({...action.order, id: action.id}),
-                error: null
-            };
-        case actions.PURCHASE_FAILED:
-            return {
-                ...state,
-                error: action.error
-            };
-        default:
-            return state
+        case actions.FETCH_ORDERS: return fetchOrders(state, action);
+        case actions.FETCH_ORDERS_ERROR: return fetchOrdersError(state, action);
+        case actions.PURCHASE_SUCCESS: return purchaseSuccess(state, action);
+        case actions.PURCHASE_FAILED: return purchaseFailed(state, action);
+        default: return state
     }
 }
